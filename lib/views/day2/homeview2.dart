f50ed712d799c13d9f4878d7bd439600b8638a78
pcharlesme <opeyemicharlese@gmail.com>
2023-01-02 00:36:41 +0100

    add features

--- lib/views/day2/homeview2.dart
+++ lib/views/day2/homeview2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:testappbc/views/day2/intropage1.dart';
 import 'package:testappbc/views/day2/intropage2.dart';
@@ -13,6 +14,10 @@ class Day2OnboardView extends StatefulWidget {
 
 class _Day2OnboardViewState extends State<Day2OnboardView> {
   PageController _controller = PageController();
+
+  //keep track on the page
+
+  bool onnLastPage = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,15 +26,74 @@ class _Day2OnboardViewState extends State<Day2OnboardView> {
           //pageview
           PageView(
             controller: _controller,
-            children: [
+            onPageChanged: (index) {
+              setState(() {
+                onnLastPage = (index == 2);
+              });
+            },
+            children: const [
               Intropage1(),
               Intropage2(),
               Intropage3(),
             ],
           ),
           Container(
-              alignment: const Alignment(0, 0.7),
-              child: SmoothPageIndicator(controller: _controller, count: 3))
+              alignment: const Alignment(0, 0.85),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  //skip
+                  GestureDetector(
+                    onTap: () {
+                      _controller.jumpToPage(2);
+                    },
+                    child: Text(
+                      "Skip",
+                      style: GoogleFonts.encodeSans(
+                          textStyle: const TextStyle(
+                        color: Colors.black87,
+                        fontSize: 14,
+                      )),
+                    ),
+                  ),
+
+                  //dot indicator
+                  SmoothPageIndicator(controller: _controller, count: 3),
+//nnext or Done
+
+                  onnLastPage
+                      ? GestureDetector(
+                          onTap: () {
+                            _controller.nextPage(
+                                duration: Duration(milliseconds: 500),
+                                curve: Curves.bounceIn);
+                          },
+                          child: Text(
+                            "Get Started",
+                            style: GoogleFonts.encodeSans(
+                                textStyle: const TextStyle(
+                              color: Colors.black87,
+                              fontSize: 14,
+                            )),
+                          ),
+                        )
+                      : GestureDetector(
+                          onTap: () {
+                            _controller.nextPage(
+                                duration: Duration(milliseconds: 500),
+                                curve: Curves.bounceIn);
+                          },
+                          child: Text(
+                            "Next",
+                            style: GoogleFonts.encodeSans(
+                                textStyle: const TextStyle(
+                              color: Colors.black87,
+                              fontSize: 14,
+                            )),
+                          ),
+                        )
+                ],
+              ))
         ],
       ),
     );
