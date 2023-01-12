325d8bf11bb18691925ca04639c27dbfe7602bc4
pcharlesme <opeyemicharlese@gmail.com>
2023-01-12 08:55:35 +0100

    UserSearch

---
+++ lib/views/day5/models.dart
@@ -0,0 +1,8 @@
+class User {
+  final String name;
+  final String username;
+  final String images;
+  bool isFollowedByMe;
+
+  User(this.name, this.username, this.images, this.isFollowedByMe);
+}
